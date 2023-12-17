bedf9b0beaa5a7027d93b5f8bf87bc20edc8c0bc
nonolin1128 <taiyijob@gmail.com>
2023-12-18 04:30:54 +0800

    update the user's profile information.

--- src/hooks/use-mocked-user.js
+++ src/hooks/use-mocked-user.js
@@ -7,13 +7,22 @@ import { _mock } from 'src/_mock';
 // const { user } = useMockedUser();
 
 // TO:
-// import { useAuthContext } from 'src/auth/hooks';
+import { useAuthContext } from 'src/auth/hooks';
 // const { user } = useAuthContext();
 
 // ----------------------------------------------------------------------
 
 export function useMockedUser() {
-  const user = {
+  // 使用 useAuthContext 取得user身分驗證
+  const { user } = useAuthContext();
+
+  // 如果已驗證，返回user資料
+  return { user: user || getDefaultMockedUser() };
+}
+
+// 預設模擬訊息
+function getDefaultMockedUser() {
+  return {
     id: '8864c717-587d-472a-929a-8e5f298024da-0',
     displayName: 'Jaydon Frankie',
     email: 'demo@minimals.cc',
@@ -29,6 +38,4 @@ export function useMockedUser() {
     role: 'admin',
     isPublic: true,
   };
-
-  return { user };
 }
